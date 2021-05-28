49b60c777c17f989ae61ef7d7163991898666885
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-05-28 19:28:37 +0200

    feat(Events.js): create associated file

---
+++ frontoffice/src/layout/Events.js
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Container } from '@material-ui/core';
+
+const Event = () => (
+  <Container>
+      Evènements
+  </Container>
+);
+
+export default Event;
--- frontoffice/src/pages/HomePage.js
+++ frontoffice/src/pages/HomePage.js
@@ -1,9 +1,25 @@
 import * as React from "react";
 import { Container } from '@material-ui/core';
+import Events from '../layout/Events.js';
 
 const HomePage = () => (
   <>
-    <Container>Voyager, Essaimer, je sais plus quoi</Container>
+    <Container maxWidth="lg">
+      Voyager, Essaimer, je sais plus quoi
+    </Container>
+    <Container maxWidth="lg">
+      Liste des lieux
+    </Container>
+    <Container maxWidth="lg">
+      Objectifs
+    </Container>
+    <Container maxWidth="lg">
+      Parcours
+    </Container>
+    <Container maxWidth="lg">
+      liens vers les types de parcours
+    </Container>
+    <Events />
   </>
 );
 
