53737d9ff27e3b6cf7651b93cef7c3ef67baa846
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-05-28 19:03:15 +0200

    feat(pages): create folder and put HomePage into

--- frontoffice/src/App.js
+++ frontoffice/src/App.js
@@ -6,7 +6,7 @@ import i18nProvider from './config/i18nProvider';
 import dataProvider from './config/dataProvider';
 import * as resources from './resources';
 
-import HomePage from "./layout/HomePage";
+import HomePage from "./pages/HomePage";
 import Layout from "./layout/Layout";
 import theme from "./layout/theme";
 
