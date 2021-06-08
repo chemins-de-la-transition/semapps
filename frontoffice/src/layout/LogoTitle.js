29175c9fb1c90f820039d9cbdff343c7818f2b10
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-08 15:46:28 +0200

    fix(Logo): add temp logo

--- frontoffice/src/layout/LogoTitle.js
+++ frontoffice/src/layout/LogoTitle.js
@@ -34,7 +34,7 @@ const LogoTitle = ({ title,classes, ...other }) => {
         <Box height={logoSize} width="200px" flexShrink={0} {...other}>
             <Link to="/" className={classes.menuLink}>
                 <Box display="flex" alignItems="center">
-                <img src={process.env.PUBLIC_URL + '/images/Logo CDLT.png'} alt="logo" className={classesLogo.logo} />
+                <img src={process.env.PUBLIC_URL + '/Logo CDLT.png'} alt="logo" className={classesLogo.logo} />
                 <Typography className={classesLogo.logoText} component="div">{title}</Typography>
                 </Box>
             </Link>
