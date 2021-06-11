afe1efe091f2add8f09a8ac296b7468e454f4f6b
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-11 15:44:00 +0200

    fix(PlacesList): ideas for places

--- frontoffice/src/pages/homepage/PlacesList.js
+++ frontoffice/src/pages/homepage/PlacesList.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { /*useMediaQuery,*/ makeStyles, Typography} from '@material-ui/core';
 import Fullwidthbox from '../../layout/Fullwidthbox';
 import Largecontainer from '../../layout/Largecontainer';
+import {List, Datagrid, TextField, DateField, BooleanField, useShowController, ShowContextProvider} from 'react-admin';
 
 const useStyles = makeStyles((theme) =>({
   background: {
@@ -10,9 +11,16 @@ const useStyles = makeStyles((theme) =>({
   }
 }));
 
-const PlacesList = ({ theme}) => {
+const PlacesList = ({theme}) => {
   const classes = useStyles();
   // const xs = useMediaQuery(theme => theme.breakpoints.down('xs'));
+
+  // const config = {
+  //   basePath: '/Page',
+  //   id: process.env.REACT_APP_LOCAL_GROUP_CODS + 'pages/bienvenue',
+  //   resource: 'Page'
+  // };
+  // todo check these paths
   return (
     <Fullwidthbox className={classes.background}>
       <Largecontainer>
@@ -25,6 +33,18 @@ const PlacesList = ({ theme}) => {
         <Typography variant="body2" component="div">
           Partez à la découvertes de lieux inspirants et allez à la rencontre de personnes qui ont choisis d’être acteurs de la transition. 
         </Typography>
+
+        {/* <ShowContextProvider value={useShowController(config)}>
+          <List {...config}>
+              <Datagrid>
+                  <TextField source="id" />
+                  <TextField source="title" />
+                  <DateField source="published_at" />
+                  <TextField source="category" />
+                  <BooleanField source="commentable" />
+              </Datagrid>
+          </List>
+      </ShowContextProvider> */}
       </Largecontainer>
     </Fullwidthbox>
   );
