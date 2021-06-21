75495ab47c6f07196bbb3f8fccb39bf87d99f7bf
srosset81 <srosset81@gmail.com>
2021-06-21 12:01:52 +0200

    Temporary access to non-admin

--- backoffice/src/config/authProvider.js
+++ backoffice/src/config/authProvider.js
@@ -8,7 +8,7 @@ const ADMIN_TYPE_URL = urlJoin(process.env.REACT_APP_MIDDLEWARE_URL, 'types', 'a
 const authProvider = semappsAuthProvider({
   middlewareUri: process.env.REACT_APP_MIDDLEWARE_URL,
   allowAnonymous: false,
-  checkUser: userData => Array.isArray(userData['pair:hasType']) ? userData['pair:hasType'].includes(ADMIN_TYPE_URL) : userData['pair:hasType'] === ADMIN_TYPE_URL,
+  checkUser: userData => true, // Array.isArray(userData['pair:hasType']) ? userData['pair:hasType'].includes(ADMIN_TYPE_URL) : userData['pair:hasType'] === ADMIN_TYPE_URL,
   httpClient,
   checkPermissions: true,
   resources: Object.fromEntries(Object.entries(resources).map(([k, v]) => [k, v.dataModel]))
