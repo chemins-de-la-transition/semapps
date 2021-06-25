473a1bdc1bc7a70b6aeb1b6aa5785c7f5bd0f7a7
Jérémy Dufraisse <jeremy.dufraisse-info@orange.fr>
2021-06-25 10:15:15 +0200

    feat(BaeList): display theme label

--- frontoffice/src/pages/HomePage/BaseList.js
+++ frontoffice/src/pages/HomePage/BaseList.js
@@ -3,7 +3,7 @@ import { makeStyles, Typography, Card, CardContent, CardHeader, CardMedia, Box, 
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import FullWidthBox from '../../layout/FullWidthBox';
 import LargeContainer from '../../layout/LargeContainer';
-import { ListBase, useListContext } from 'react-admin';
+import { ListBase, useListContext,useGetOne , Loading} from 'react-admin';
 import { Link } from 'react-router-dom';
 import PlaceOutlinedIcon from '@material-ui/icons/PlaceOutlined';
 
@@ -30,7 +30,8 @@ const useStyles = makeStyles((theme) =>({
     padding: '0',
     display: 'flex',
     justifyContent: 'center',
-    '& .MuiTypography-root':{
+    alignItems: 'center',
+    '& .block':{
       marginLeft: '16px',
       marginRight: '16px',
       background: theme.palette.secondary.main,
@@ -44,7 +45,18 @@ const useStyles = makeStyles((theme) =>({
       top: '-14px',
       width: '90%',
       overflow: 'clip',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
     },
+    '& .MuiTypography-root':{
+      background: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+      fontSize: '12px',
+      lineHeight: '12px',
+      textAlign: 'center',
+      textDecoration: 'none',
+    }
   },
   link: {
     alignSelf: 'flex-end',
@@ -115,48 +127,103 @@ const SubHeader = ({data,id}) => {
   );
 };
 
-const ItemsGrid = () => {
-  const classes = useStyles();
-  const { ids, data , basePath } = useListContext();
+const GetOneThemeLabel = ({id}) => {
+  const { data, loading, error} = useGetOne('Theme', id);
+  if (loading) { return <Loading />; }
+  if (error) { return <p>ERROR</p>; }
   return (
-    <div className={classes.cardContainer}>
-    {ids.map(id =>
-        <Card key={id} className={classes.cardClass}>
-          <CardActionArea>
-            <CardMedia
-                className={classes.media+' '+classes.noDecoration}
-                image={data[id]["pair:image"]}
-                title={data[id]["pair:label"]}
-                to={basePath+'/'+encodeURIComponent(id)+'/show'}
-                component={Link}
-              />
-          </CardActionArea>
-          {
-            (data[id]["pair:hasTopic"]) 
+    <span>{data["pair:label"]}</span>
+  ) ;
+};
+
+const CardBlock = ({id,data,basePath}) => {
+  const classes = useStyles();
+  let topics = data[id]["pair:hasTopic"];
+  if (!Array.isArray(topics)) {
+    topics = [topics];
+  }
+  // shuffle topics
+  topics.sort(() => Math.random() - 0.5);
+  const firstTopic = topics[0];
+  const secondTopic = (topics.length > 1) ? topics[1] : null;
+  return (
+    <Card key={id} className={classes.cardClass}>
+      <CardActionArea>
+        <CardMedia
+            className={classes.media+' '+classes.noDecoration}
+            image={data[id]["pair:image"]}
+            title={data[id]["pair:label"]}
+            to={basePath+'/'+encodeURIComponent(id)+'/show'}
+            component={Link}
+          />
+      </CardActionArea>
+      {
+        (firstTopic) 
+        ? (
+            (secondTopic)
             ? (
               <CardContent
                 className={classes.cardTheme + ' '+classes.noDecoration}
-                component={Link}
-                to={'/theme/'+encodeURIComponent(data[id]["pair:hasTopic"])+'/show'}
                 >
-                <Typography variant="body2">{data[id]["pair:hasTopic"]}</Typography>
-              </CardContent>)
-            : ''
-          }
-          <CardHeader
-              to={basePath+'/'+encodeURIComponent(id)+'/show'}
-              className={classes.noDecoration}
+                <Box class="block" className={classes.noDecoration}>
+                  <Typography variant="body2" 
+                  component={Link}
+                  to={'/theme/'+encodeURIComponent(firstTopic)+'/show'}>
+                    <GetOneThemeLabel id={firstTopic}></GetOneThemeLabel>
+                  </Typography>
+                  <Typography variant="body2" 
+                  component={Link}
+                  to={'/theme/'+encodeURIComponent(secondTopic)+'/show'}>
+                    /<GetOneThemeLabel id={secondTopic}></GetOneThemeLabel>
+                  </Typography>
+                </Box>
+              </CardContent>
+            )
+            :
+            <CardContent
+              className={classes.cardTheme + ' '+classes.noDecoration}
               component={Link}
-              title={<Typography className={classes.cardTitle} variant="h4" color="primary">{data[id]["pair:label"]}</Typography>}
-              subheader={<SubHeader data={data} id={id}></SubHeader>}
-          />
-          <CardContent
-            to={basePath+'/'+encodeURIComponent(id)+'/show'}
-            className={classes.noDecoration + ' '+ classes.comment}
-            component={Link}>
-              <Typography variant="body2" color="secondary" component="div">{data[id]["pair:comment"]}</Typography>
-          </CardContent>
-        </Card>
+              to={'/theme/'+encodeURIComponent(firstTopic)+'/show'}
+              >
+              <Typography class="block" variant="body2"><GetOneThemeLabel id={firstTopic}></GetOneThemeLabel></Typography>
+            </CardContent>
+          )
+        : ''
+      }
+      <CardHeader
+          to={basePath+'/'+encodeURIComponent(id)+'/show'}
+          className={classes.noDecoration}
+          component={Link}
+          title={<Typography className={classes.cardTitle} variant="h4" color="primary">{data[id]["pair:label"]}</Typography>}
+          subheader={<SubHeader data={data} id={id}></SubHeader>}
+      />
+      <CardContent
+        to={basePath+'/'+encodeURIComponent(id)+'/show'}
+        className={classes.noDecoration + ' '+ classes.comment}
+        component={Link}>
+          <Typography variant="body2" color="secondary" component="div">{data[id]["pair:comment"]}</Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+const ItemsGrid = ({nb}) => {
+  const classes = useStyles();
+  const { ids, data , basePath } = useListContext();
+  // shuffle ids
+  let shuffledIds = ids;
+  shuffledIds.sort(() => Math.random() - 0.5);
+  shuffledIds.sort(() => Math.random() - 0.5);
+  // keep only nb
+  shuffledIds = shuffledIds.slice(0,nb);
+  return (
+    <div className={classes.cardContainer}>
+    {shuffledIds.map(id =>
+        <CardBlock
+          id={id}
+          data={data}
+          basePath={basePath}
+          ></CardBlock>
     )}
     </div>
   );
@@ -191,12 +258,12 @@ const BaseList = ({resource,basePath,title,subtitle,headComment,linkText}) => {
           <ListBase
             resource={resource}
             basePath={basePath}
-            perPage={4}
+            // perPage={4}
             // filter={{ 'pair:hasStatus': process.env.REACT_APP_MIDDLEWARE_URL + 'status/en-vedette' }}
-            sort={{ field: 'published', order: 'DESC' }}
+            // sort={{ field: 'published', order: 'DESC' }}
             className={classes.ListBase}
             >
-              <ItemsGrid />
+              <ItemsGrid nb={4}/>
           </ListBase>
       </LargeContainer>
     </FullWidthBox>
