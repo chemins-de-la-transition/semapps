a5ec3a497f5d6fca8c92c1e4f53e19f9acd71702
Mathieu-CdlT <61470357+Mathieu-CdlT@users.noreply.github.com>
2021-10-05 17:33:20 +0200

    Update HomePage.js

--- frontoffice/src/pages/HomePage/HomePage.js
+++ frontoffice/src/pages/HomePage/HomePage.js
@@ -15,7 +15,7 @@ const HomePage = () => (
       basePath="/Place"
       title="Les lieux"
       subtitle="A visiter"
-      headComment="Partez à la découvertes de lieux inspirants et allez à la rencontre de personnes qui ont choisis d’être acteurs de la transition."
+      headComment="Partez à la découverte de lieux inspirants et allez à la rencontre de personnes qui ont choisi d’être actrices de la transition."
       linkText="Voir tous les lieux"
       CardSubHeaderComponent={PlaceSubHeader}
     />
@@ -25,7 +25,7 @@ const HomePage = () => (
       basePath="/Course"
       title="Les parcours"
       subtitle="Thématiques & géographiques"
-      headComment="Tu rêves de partir sur les routes pour découvrir des savoirs faire ou même apprendre un métier sur le terrain? Découvre nos parcours."
+      headComment="Tu rêves de partir sur les routes pour découvrir des savoir-faire ou même apprendre un métier sur le terrain? Découvre nos parcours."
       linkText="Voir tous les parcours"
       CardSubHeaderComponent={CourseSubHeader}
     />
