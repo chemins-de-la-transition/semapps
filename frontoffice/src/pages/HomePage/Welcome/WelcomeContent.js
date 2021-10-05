8349e6bf7d652c6c30385400a7c7efa7a78aefb4
Sébastien Rosset <srosset81@gmail.com>
2021-10-05 17:03:15 +0200

    Update WelcomeContent.js

--- frontoffice/src/pages/HomePage/Welcome/WelcomeContent.js
+++ frontoffice/src/pages/HomePage/Welcome/WelcomeContent.js
@@ -70,7 +70,7 @@ const WelcomeContent = () => {
           </Typography>
           <div className={classes.textBar} />
           <Typography variant="body1" className={classes.subtitleText} component="div">
-            Pour les curieux qui souhaitent découvrir une région et apprendre en voyageant, nous vous proposons des
+            Pour les curieuses et les curieux qui souhaitent découvrir une région et apprendre en voyageant, nous vous proposons des
             itinéraires éco-touristiques autour des métiers de la transition, conçus pour être parcourus à pied ou à
             vélo. Partez à la rencontre des acteurs de lieux inspirationnels et découvrez leurs savoirs-faire.
           </Typography>
