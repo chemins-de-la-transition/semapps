74f43ad48420a5caff6b609bc52e98ea819cf3be
srosset81 <srosset81@gmail.com>
2021-10-06 15:42:10 +0200

    Closes #126

--- frontoffice/src/layout/SideMenu.js
+++ frontoffice/src/layout/SideMenu.js
@@ -5,7 +5,7 @@ import { MenuItemLink } from 'react-admin';
 const SideMenu = ({ menuItems, sidebarOpen, setSidebarOpen }) => (
   <Drawer variant="temporary" open={sidebarOpen} onClose={() => setSidebarOpen(false)}>
     {menuItems.map((menuItem) => (
-      <MenuItemLink key={menuItem.link} to={menuItem.link} primaryText={menuItem.name} />
+      <MenuItemLink key={menuItem.link} to={menuItem.link} primaryText={menuItem.name} onClick={() => setSidebarOpen(false)} />
     ))}
   </Drawer>
 );
