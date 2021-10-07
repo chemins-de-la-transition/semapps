0ea46d6152b33b275d61bef0c8c3f966a0b91bc9
srosset81 <srosset81@gmail.com>
2021-10-07 12:54:10 +0200

    Closes #131

--- backoffice/src/resources/Agent/Activity/Event/EventList.js
+++ backoffice/src/resources/Agent/Activity/Event/EventList.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import { Datagrid, DateField, EditButton, ShowButton, TextField } from 'react-admin';
 import { MultiViewsList } from '@semapps/archipelago-layout';
 import { ListWithPermissions } from '@semapps/auth-provider';
-import { CalendarList, DaysList } from '@semapps/date-components';
+import { CalendarList } from '@semapps/date-components';
 import frLocale from '@fullcalendar/core/locales/fr';
 import ListIcon from '@material-ui/icons/List';
 import EventIcon from '@material-ui/icons/Event';
@@ -10,6 +11,21 @@ const EventList = props => (
   <MultiViewsList
     ListComponent={ListWithPermissions}
     views={{
+      list: {
+        label: 'Liste',
+        icon: ListIcon,
+        perPage: 1000,
+        pagination: false,
+        sort: { field: 'pair:startDate', order: 'ASC' },
+        list: (
+          <Datagrid rowClick="show">
+            <TextField source="pair:label" />
+            <DateField source="pair:startDate" />
+            <ShowButton />
+            <EditButton />
+          </Datagrid>
+        )
+      },
       calendar: {
         label: 'Calendrier',
         icon: EventIcon,
@@ -17,21 +33,6 @@ const EventList = props => (
         pagination: false,
         list: (
           <CalendarList
-            locale={frLocale}
-            label="pair:label"
-            startDate="pair:startDate"
-            endDate="pair:endDate"
-            linkType="show"
-          />
-        )
-      },
-      list: {
-        label: 'Liste',
-        icon: ListIcon,
-        perPage: 1000,
-        pagination: false,
-        list: (
-          <DaysList
             locale={frLocale}
             label="pair:label"
             startDate="pair:startDate"
