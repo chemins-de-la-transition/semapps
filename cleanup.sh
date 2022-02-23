ffc892565be59994012a086b461a54d3bd27e4c1
srosset81 <srosset81@gmail.com>
2022-02-23 11:15:03 +0100

    Cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -2,5 +2,4 @@ docker-compose stop fuseki
 docker-compose up fuseki_compact
 docker-compose up -d fuseki
 
-sudo find ./data/fuseki/databases/localData -iname 'Data*' ! -wholename $(find ./data/fuseki/databases/localData -iname 'Data*' -type d | sort -n | tail -n 1)  -type d
-sudo find ./data/fuseki/databases/localDataAcl -iname 'Data*' ! -wholename $(find ./data/fuseki/databases/localDataAcl -iname 'Data*' -type d | sort -n | tail -n 1)  -type d
+sudo find ./data/fuseki/databases/localData -iname 'Data*' ! -wholename $(find ./data/fuseki/databases/localData -iname 'Data*' -type d | sort -n | tail -n 1)  -type d  -exec rm -rf {} +
