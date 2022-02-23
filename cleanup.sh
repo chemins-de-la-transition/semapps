7a3ba94f4d2031c76f3897f11f7bb8fd4e9037dd
srosset81 <srosset81@gmail.com>
2022-02-23 11:34:44 +0100

    Fix

--- cleanup.sh
+++ cleanup.sh
@@ -1,4 +1,9 @@
+# On each restart, Fuseki will compact
 docker-compose down
 docker-compose up -d
 
+# Wait for the compacting to be executed
+sleep 5
+
+# Delete the old datasets
 sudo find ./data/fuseki/databases/localData -iname 'Data*' ! -wholename $(find ./data/fuseki/databases/localData -iname 'Data*' -type d | sort -n | tail -n 1)  -type d  -exec rm -rf {} +
