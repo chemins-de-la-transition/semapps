32187b1af603337566ea8a52d7756b427905c4a6
srosset81 <srosset81@gmail.com>
2022-04-07 13:10:43 +0200

    New contributor type

--- backoffice/src/config/authProvider.js
+++ backoffice/src/config/authProvider.js
@@ -3,14 +3,15 @@ import { authProvider as semappsAuthProvider } from '@semapps/auth-provider';
 import { httpClient } from '@semapps/semantic-data-provider';
 
 const ADMIN_TYPE_URL = urlJoin(process.env.REACT_APP_MIDDLEWARE_URL, 'types', 'admin');
+const CONTRIBUTOR_TYPE_URL = urlJoin(process.env.REACT_APP_MIDDLEWARE_URL, 'types', 'contributor');
 
 const authProvider = semappsAuthProvider({
   middlewareUri: process.env.REACT_APP_MIDDLEWARE_URL,
   allowAnonymous: false,
   checkUser: (userData) =>
     Array.isArray(userData['pair:hasType'])
-      ? userData['pair:hasType'].includes(ADMIN_TYPE_URL)
-      : userData['pair:hasType'] === ADMIN_TYPE_URL,
+      ? userData['pair:hasType'].includes(ADMIN_TYPE_URL) || userData['pair:hasType'].includes(CONTRIBUTOR_TYPE_URL)
+      : userData['pair:hasType'] === ADMIN_TYPE_URL || userData['pair:hasType'] === CONTRIBUTOR_TYPE_URL,
   httpClient,
   checkPermissions: true
 });
