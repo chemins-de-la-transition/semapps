edfbf1c3c3094df91b4c349e0ac6caf9be32bd8f
Vincentfarcy <v.farcy72@gmail.com>
2022-04-17 22:04:08 +0200

    frontoffice : duplicate event

--- frontoffice/src/resources/Activity/Event/EventForm.js
+++ frontoffice/src/resources/Activity/Event/EventForm.js
@@ -1,20 +1,114 @@
-import React from 'react';
-import { BooleanInput, SimpleForm, ImageInput, TextInput, useGetIdentity, required, email } from 'react-admin';
+import React, { useCallback, useEffect, useState } from 'react';
+import {
+  BooleanInput,
+  Error,
+  ImageInput,
+  Link,
+  Loading,
+  SelectInput,
+  SimpleForm,
+  TextInput,
+  email,
+  required,
+  useDataProvider,
+  useGetIdentity,
+} from 'react-admin';
 import { MarkdownInput } from '@semapps/markdown-components';
 import { ImageField } from '@semapps/semantic-data-provider';
 import { DateTimeInput } from '@semapps/date-components';
 import { ThemesInput, TypeInput, SkillsInput } from '../../../pair';
 import frLocale from 'date-fns/locale/fr';
+// import { Grow } from '@material-ui/core';
 
 const EventForm = ({ mode, ...rest }) => {
   const { identity } = useGetIdentity();
+  const dataProvider = useDataProvider();
+  const [eventsList, setEventsList] = useState([]);
+  const [eventsListIsLoading, setEventsListIsLoading] = useState(true);
+  const [eventsListIsOnError, setEventsListIsOnError] = useState(false);
+  const [chosenEvent, setChosenEvent] = useState(null);
+  const [duplicateIsOpen, setDuplicateIsOpen] = useState(false);
+    
+  const getAllEvents = useCallback(() => {
+    if (eventsList.length === 0) {
+      dataProvider
+        .getList('Event')
+        .then(({ data }) => {
+          setEventsList(data)
+          setEventsListIsLoading(false)
+        })
+        .catch(e => {
+          setEventsListIsLoading(false)
+          setEventsListIsOnError(true)
+        })
+    }
+  }, [dataProvider, eventsList, setEventsList, setEventsListIsLoading, setEventsListIsOnError]);
+  
+  const handleClickToggleDuplicate = useCallback((duplicateIsOpen) => (e) => {
+    e.preventDefault();
+    setDuplicateIsOpen(! duplicateIsOpen)
+    setChosenEvent(null)
+  }, [setDuplicateIsOpen]);
+  
+  const handleChangeSelectEvent = useCallback((eventsList) => (e) => {
+    const eventId = e.target.value;
+    const chosenEvent = eventsList.find( event => event.id === eventId )
+    setChosenEvent(chosenEvent)
+    setDuplicateIsOpen(false)
+  }, [setChosenEvent]);
+    
+  useEffect(() => {
+    getAllEvents();
+  }, [getAllEvents]);
+  
+  const getFormatedEvent = useCallback((chosenEvent) => {
+    let formatedEvent = {};  
+    for (const property in chosenEvent) {
+      if (! ['pair:label'].includes(property)) {
+        formatedEvent = { ...formatedEvent, [property]: chosenEvent[property] };
+      }
+    }
+    return formatedEvent
+  }, []);
+  
+  const initalValues = (mode) => {
+    switch (mode) {
+      case 'create': return { 'cdlt:organizedBy': identity?.id }
+      case 'duplicate': return getFormatedEvent(chosenEvent)
+      default: return undefined
+    }
+  }
+  
+  if (chosenEvent) {
+    mode = 'duplicate';
+  }
+
   return (
     <SimpleForm
-      initialValues={mode === 'create' ? { 'cdlt:organizedBy': identity?.id } : undefined}
+      initialValues={ initalValues(mode) }
       {...rest}
       redirect="/MyEvents"
     >
       <TextInput source="pair:label" fullWidth validate={[required()]} />
+      
+      <Link onClick={ handleClickToggleDuplicate(duplicateIsOpen) }>Dupliquer un événement existant ?</Link>
+      { duplicateIsOpen && eventsListIsLoading &&
+        /*todo*/
+        <Loading/>
+      }
+      { duplicateIsOpen && ! eventsListIsLoading && eventsListIsOnError &&
+        /*todo*/
+        <Error/>
+      }
+      { duplicateIsOpen && ! eventsListIsLoading && ! eventsListIsOnError &&
+        <>
+          <SelectInput label="Choisissez un événement" source="eventsList" fullWidth 
+            choices={ eventsList.map(event => ({ id: event.id, name: event["pair:label"] })) }
+            onChange={ handleChangeSelectEvent(eventsList) }
+          />
+        </>
+      }
+
       <TextInput source="pair:comment" fullWidth validate={[required()]} />
       <DateTimeInput
         source="pair:startDate"
