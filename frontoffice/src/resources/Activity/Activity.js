044fbf87dafc4c73e99ebad8dd0e28eb96f00b92
Vincentfarcy <v.farcy72@gmail.com>
2022-05-17 22:01:33 +0200

    activity redirect to event or course

--- frontoffice/src/resources/Activity/Activity.js
+++ frontoffice/src/resources/Activity/Activity.js
@@ -1,8 +1,23 @@
+import React from 'react';
+import { Show } from 'react-admin';
+import { RedirectByType } from '@semapps/archipelago-layout';
 import ExploreIcon from '@material-ui/icons/Explore';
+
+const ActivityRedirect = (props) => (
+  <Show {...props}>
+    <RedirectByType
+      typesMap={{
+        Course: 'cdlt:Course',
+        Event: 'pair:Event',
+      }}
+    />
+  </Show>
+);
 
 export default {
   config: {
     icon: ExploreIcon,
+    show: ActivityRedirect,
     options: {
       label: 'Voyages',
     },
