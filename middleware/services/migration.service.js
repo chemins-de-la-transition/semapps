2d23b09e40582d4f0eb476d7a3b8c3d92c29bc84
Vincentfarcy <v.farcy72@gmail.com>
2022-06-01 16:43:35 +0200

    migration of write permissions

--- middleware/services/migration.service.js
+++ middleware/services/migration.service.js
@@ -38,6 +38,71 @@ module.exports = {
           this.logger.info('Event ' + eventUri + ' has no location, skipping...');
         }
       }
+    },
+    async migrateImagePredicates(ctx) {
+      await this.actions.replacePredicate({ oldPredicate: 'http://virtual-assembly.org/ontologies/pair#image', newPredicate: 'http://virtual-assembly.org/ontologies/pair#depictedBy' }, { parentCtx: ctx });
+      await this.actions.replacePredicate({ oldPredicate: 'http://virtual-assembly.org/ontologies/pair#isDepictedBy', newPredicate: 'http://virtual-assembly.org/ontologies/pair#depictedBy' }, { parentCtx: ctx });
+    },
+    async setWritePermissionsToCreator(ctx) {
+      console.log('*** setWritePermissionsToCreator ***');
+      await this.setWritePermissionsToCreatorForASingleResource(ctx, 'places');
+      await this.setWritePermissionsToCreatorForASingleResource(ctx, 'events');
+      await this.setWritePermissionsToCreatorForASingleResource(ctx, 'courses');
+      await this.setWritePermissionsToCreatorForASingleResource(ctx, 'paths');
+      await this.setWritePermissionsToCreatorForASingleResource(ctx, 'organizations');
+    }
+  },
+  methods: {
+    async setWritePermissionsToCreatorForASingleResource(ctx, resourceSlug) {
+      
+      const resources = await ctx.call('ldp.container.getUris', { containerUri: urlJoin(CONFIG.HOME_URL, resourceSlug) });
+      console.log('********************************************************');      
+      console.log('****** resource: ', resourceSlug, 'nb:', resources.length);      
+      console.log('********************************************************');      
+
+      let i=0;
+      for( let resourceUri of resources ) {
+        
+        i++;
+        console.log('****** ', i);
+        console.log('          ', resourceUri);
+        
+        const resource = await this.broker.call('ldp.resource.get', {
+          resourceUri: resourceUri,
+          accept: MIME_TYPES.JSON,
+          webId: 'system'
+        });
+
+        await ctx.call('webacl.resource.removeRights', {
+          webId: 'system',
+          resourceUri: resource.id,
+          rights: {
+            anon: {
+              write: true
+            },
+            anyUser: {
+              write: true
+            }
+          }
+        });
+        console.log('           removeRights (anon and anyUser)');
+        
+        if (!resource['dc:creator']) {
+          console.log(' x         => No creator !');
+        } else {
+          await ctx.call('webacl.resource.addRights', {
+            webId: 'system',
+            resourceUri: resource.id,
+            additionalRights: {
+              user: {
+                uri: resource['dc:creator'],
+                write: true
+              }
+            }
+          });
+          console.log('           addRights, user:', resource['dc:creator']);
+        }
+      }
     }
   }
 }
