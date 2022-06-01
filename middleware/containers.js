4b5a3be853ffc234fd20d3cd31fc08f218dad8c9
srosset81 <srosset81@gmail.com>
2022-06-01 19:31:18 +0200

    Fix permissions for superadmins

--- middleware/containers.js
+++ middleware/containers.js
@@ -38,10 +38,33 @@ const defaultWritePermissionToContributors = {
   }
 };
 
+const rootPermissions = {
+  anon: {
+    read: true,
+  },
+  group: {
+    uri: urlJoin(CONFIG.HOME_URL, '_groups', 'superadmins'),
+    read: true,
+    write: true,
+    control: true
+  },
+  default: {
+    anon: {
+      read: true
+    },
+    group: {
+      uri: urlJoin(CONFIG.HOME_URL, '_groups', 'superadmins'),
+      read: true,
+      write: true,
+      control: true
+    }
+  }
+};
+
 module.exports = [
   {
     path: '/',
-    permissions: anonReadPermission,
+    permissions: rootPermissions,
   },
   {
     path: '/organizations',
