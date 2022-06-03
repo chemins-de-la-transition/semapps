0ac7b171f07d47c868b3e769c9d65d0ab1d3deb8
Vincentfarcy <v.farcy72@gmail.com>
2022-06-03 11:37:05 +0200

    list filters

--- frontoffice/src/resources/Activity/Course/CourseList.js
+++ frontoffice/src/resources/Activity/Course/CourseList.js
@@ -14,7 +14,7 @@ const CourseList = (props) => {
       filters={[
         <SearchFilter />,
         <Filter reference="Region" source="cdlt:hasRegion" inverseSource="cdlt:regionOf" label="Région" />,
-        <Filter reference="Theme" source="pair:hasTopic" inverseSource="pair:topicOf" label="Secteur d'activité" />,
+        <Filter reference="Sector" source="pair:hasSector" inverseSource="pair:sectorOf" label="Secteur d'activité" />,
         <Filter
           reference="Type"
           source="cdlt:hasCourseType"
