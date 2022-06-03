fbd82c8598f87bff84af0ad95329548dbc9d63a6
Vincentfarcy <v.farcy72@gmail.com>
2022-06-03 21:26:54 +0200

    replace image components by depictedBy Field

--- frontoffice/src/commons/lists/FeaturedList/CardBlock.js
+++ frontoffice/src/commons/lists/FeaturedList/CardBlock.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import { makeStyles, Typography, Card, CardContent, CardHeader, CardMedia, CardActionArea, Chip } from '@material-ui/core';
-import { TextField, FunctionField } from 'react-admin';
+import { ImageField, TextField, SingleFieldList } from 'react-admin';
 import { SeparatedListField } from '@semapps/archipelago-layout';
 import { ReferenceArrayField } from '@semapps/semantic-data-provider';
 import { Link } from 'react-router-dom';
 import { linkToFilteredList } from "../../../utils";
 import LikeButton from '../../buttons/LikeButton';
-import AgricultureAlimentation_PictoCdlT from '../../../icons/AgricultureAlimentation_PictoCdlT.png' ;
-import Artisanat_PictoCdlT from '../../../icons/Artisanat_PictoCdlT.png' ;
-import Communication_PictoCdlT from '../../../icons/Communication_PictoCdlT.png' ;
-import CultureModeVie_PictoCdlT from '../../../icons/CultureModeVie_PictoCdlT.png' ;
-import DevTerritorial_PictoCdlT from '../../../icons/DevTerritorial_PictoCdlT.png' ;
-import Ecologie_PictoCdlT from '../../../icons/Ecologie_PictoCdlT.png' ;
-import Energie_PictoCdlT from '../../../icons/Energie_PictoCdlT.png' ;
-import Gouvernance_PictoCdlT from '../../../icons/Gouvernance_PictoCdlT.png' ;
-import Habitat_PictoCdlT from '../../../icons/Habitat_PictoCdlT.png' ;
-import JusticeSociale_PictoCdlT from '../../../icons/JusticeSociale_PictoCdlT.png' ;
-import Mobilite_PictoCdlT from '../../../icons/Mobilite_PictoCdlT.png' ;
-import Sante_Bienetre_PictoCdlT from '../../../icons/Sante_Bienetre_PictoCdlT.png' ;
-import ScienceTech_PictoCdlT from '../../../icons/ScienceTech_PictoCdlT.png' ;
 
 const useStyles = makeStyles((theme) => ({
-  cardTopics: {
+  sectorCard: {
     position: 'absolute',
     zIndex: 1,
     paddingLeft: 0,
+    '& .MuiChip-root': {
+      background: 'none',
+      maxWidth: 180,
+      '& .MuiChip-label': {
+        overflow: 'visible'
+      }
+    }
   },
-  categoryLogo: {
+  sectorImage: {
     width: 40,
     height: 40,
     background: "white",
     borderRadius: 100,
+    marginRight: 4,
+    '& img': {
+      width: '100%',
+      margin: 0
+    }
   },
   likeButton: {
     position: 'absolute',
@@ -149,41 +148,19 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-function ChoosePicture(category, className) {
-  const logo = (category==="Agriculture et alimentation") ? AgricultureAlimentation_PictoCdlT
-    : (category==="Artisanat, commerce, industrie") ? Artisanat_PictoCdlT
-    : (category==="Information, communication et médias") ? Communication_PictoCdlT
-    : (category==="Culture et modes de vie") ? CultureModeVie_PictoCdlT
-    : (category==="Développement territorial") ? DevTerritorial_PictoCdlT
-    : (category==="Ecologie - Environnement") ? Ecologie_PictoCdlT
-    : (category==="Énergie") ? Energie_PictoCdlT
-    : (category==="Gouvernance et modes d’organisation") ? Gouvernance_PictoCdlT
-    : (category==="Habitat & urbanisme") ? Habitat_PictoCdlT
-    : (category==="Justice sociale") ? JusticeSociale_PictoCdlT
-    : (category==="Mobilités") ? Mobilite_PictoCdlT
-    : (category==="Santé et bien-être") ? Sante_Bienetre_PictoCdlT
-    : (category==="Sciences et technologies") ? ScienceTech_PictoCdlT
-    : null
-  
-  return (
-    logo ? <img src={logo} alt="logo" className={className} title={category}/> : <span>{category}</span>
-  );
-}
-
 const CardBlock = ({ record, basePath, CardSubHeaderComponent, resource }) => {
   const classes = useStyles();
   return (
     <Card key={record.id} className={classes.cardClass}>
       <div style={{position:'relative'}}>
-      {record['pair:hasTopic'] && (
-        <CardContent className={classes.cardTopics}>
+      {record['pair:hasSector'] && (
+        <CardContent className={classes.sectorCard}>
             <Chip
-              style={{background:'none', maxWidth:'180px'}}
               label={
-                <ReferenceArrayField source="pair:hasTopic" reference="Theme" record={record}>
-                  <SeparatedListField separator=" " link={linkToFilteredList( 'Course', 'pair:hasTopic')}>
-                      <FunctionField label="Name" render={record => ChoosePicture(record["pair:label"],classes.categoryLogo)} />
-                  </SeparatedListField>
+                <ReferenceArrayField source="pair:hasSector" reference="Sector" record={record} perPage={3}>
+                  <SingleFieldList>
+                    <ImageField source="pair:depictedBy" title="pair:label" className={classes.sectorImage} />
+                  </SingleFieldList>
                 </ReferenceArrayField>
               }
             />
