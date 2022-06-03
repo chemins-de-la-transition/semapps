c0cf39b6be514715c9f66a7324f34bcbfc52e260
clemenceCDLT <clemence.calas.cdlt@gmail.com>
2022-06-03 15:06:34 +0200

    Fix details on homepage

--- frontoffice/src/pages/HomePage/Partners.js
+++ frontoffice/src/pages/HomePage/Partners.js
@@ -66,12 +66,11 @@ const Partners = () => {
     { 'pair:partnerOf': process.env.REACT_APP_MIDDLEWARE_URL + 'organizations/les-chemins-de-la-transition' }
   );
 
-  const sortedIds = ids.reduce((acc, id) => {
-    if (data[id]['pair:label']==="Assemblée Virtuelle") {
-      return [id, ...acc];
-    }
-    return [...acc, id];
-  }, []);
+  const sortedIds = [
+    ...ids.filter(id => data[id]['pair:label']==="Assemblée Virtuelle"),
+    ...ids.filter(id => data[id]['pair:label']==="MES Occitanie"),
+    ...ids.filter(id => !["Assemblée Virtuelle","MES Occitanie"].includes(data[id]['pair:label'])),
+  ]
 
   const classes = useStyles();
   return (
