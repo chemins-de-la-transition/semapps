855c3b4cd1e8bf1131f9e40ff4c53dac97c6a110
Vincentfarcy <v.farcy72@gmail.com>
2022-06-12 19:21:29 +0200

    courseDetails Path link

--- frontoffice/src/resources/Activity/Course/CourseDetails.js
+++ frontoffice/src/resources/Activity/Course/CourseDetails.js
@@ -63,7 +63,7 @@ const CourseDetails = (props) => {
           </ReferenceArrayField>
         }
         { (isVertical || sm ) && 
-          <ReferenceArrayField source="cdlt:courseOn" reference="Actor" icon={<ActorIcon />} label="Fait partie de">
+          <ReferenceArrayField source="cdlt:courseOn" reference="Path" icon={<PathIcon />} label="Fait partie de">
             <SeparatedListField linkType="show">
               <TextField source="pair:label" />
             </SeparatedListField>
