0e34c54104c3f64f711b397d582ec069551900d0
clemenceCDLT <clemence.calas.cdlt@gmail.com>
2022-07-11 11:08:56 +0200

    Change reference

--- frontoffice/src/resources/Registration/RegistrationCard.js
+++ frontoffice/src/resources/Registration/RegistrationCard.js
@@ -13,25 +13,13 @@ const useStyles = makeStyles((theme) => ({
 
 const RegistrationCard = ({ record }) => {
   const classes = useStyles();
-  const isEvent = record['cdlt:registrationFor']?.indexOf('/events/')!==-1;
-  const isCourse = record['cdlt:registrationFor']?.indexOf('/courses/')!==-1;
 
   return (
     <>
     {record['cdlt:registrationFor'] &&
     (
-    isEvent ?
-    <ReferenceField record={record} source="cdlt:registrationFor" reference="Event" link="show">
+    <ReferenceField record={record} source="cdlt:registrationFor" reference="LEP" link="show" >
       <TextField source="pair:label" variant="h2" className={classes.title}/>
-    </ReferenceField>
-    : 
-    isCourse ?
-    <ReferenceField record={record} source="cdlt:registrationFor" reference="Course" link="show" >
-      <TextField source="pair:label" />
-    </ReferenceField>
-    : 
-    <ReferenceField record={record} source="cdlt:registrationFor" reference="Place" link="show" >
-      <TextField source="pair:label" />
     </ReferenceField>
     )
     }
