7a6cd34867e88cbe4648fd327995eb8f7dd2a785
srosset81 <srosset81@gmail.com>
2022-12-14 18:16:26 +0100

    Enable signature authorization

--- middleware/services/api.service.js
+++ middleware/services/api.service.js
@@ -1,4 +1,5 @@
 const ApiGatewayService = require('moleculer-web');
+const { Errors: E } = require("moleculer-web");
 
 module.exports = {
   mixins: [ApiGatewayService],
@@ -11,10 +12,24 @@ module.exports = {
   },
   methods: {
     authenticate(ctx, route, req, res) {
-      return ctx.call('auth.authenticate', { route, req, res });
+      if (req.headers.signature) {
+        return ctx.call('signature.authenticate', { route, req, res });
+      } else if (req.headers.authorization) {
+        return ctx.call('auth.authenticate', { route, req, res });
+      } else {
+        ctx.meta.webId = 'anon';
+        return Promise.resolve(null);
+      }
     },
     authorize(ctx, route, req, res) {
-      return ctx.call('auth.authorize', { route, req, res });
+      if (req.headers.signature) {
+        return ctx.call('signature.authorize', { route, req, res });
+      } else if (req.headers.authorization) {
+        return ctx.call('auth.authorize', { route, req, res });
+      } else {
+        ctx.meta.webId = 'anon';
+        return Promise.reject(new E.UnAuthorizedError(E.ERR_NO_TOKEN));
+      }
     }
   }
 };
