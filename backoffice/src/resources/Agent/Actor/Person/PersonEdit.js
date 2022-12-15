e85bd6350840f39a90bc1e9534d9befb97e861e5
Guillaume <guillaume.rouyer@assemblee-virtuelle.org>
2022-12-15 11:02:59 +0100

    Update PersonEdit.js
    Ajout de labels et réagencement

--- backoffice/src/resources/Agent/Actor/Person/PersonEdit.js
+++ backoffice/src/resources/Agent/Actor/Person/PersonEdit.js
@@ -32,17 +32,22 @@ export const PersonEdit = (props) => (
     {...props}
   >
     <TabbedForm redirect="show">
-      <FormTab label="Principal">
-        <TextInput source="pair:firstName" fullWidth />
-        <TextInput source="pair:lastName" fullWidth />
-        <TextInput source="pair:alternativeLabel" fullWidth />
-        <TextInput source="pair:comment" fullWidth />
-        <MarkdownInput source="pair:description" fullWidth />
-        <TextInput source="pair:homePage" fullWidth />
-        <ImageInput source="pair:depictedBy" accept="image/*">
+      <FormTab label="A son propos">
+        <TextInput source="pair:firstName" label="Son prénom" fullWidth />
+        <TextInput source="pair:lastName" label="Son nom de famille" fullWidth />
+        <TextInput source="pair:alternativeLabel" label="Son nom d'utilisateur.trice" fullWidth />
+        <TextInput source="pair:comment" label="Qui est-il/elle en une phrase ?" fullWidth />
+        <TextInput source="pair:homePage" label="Un lien à propos de lui/elle ?" fullWidth />
+        <MarkdownInput source="pair:description" label="N\'hésite pas à le/la décrire plus longuement !"  fullWidth />
+        <MarkdownInput source="cdlt:intentions" label="Ses intentions en venant sur les chemins de la transition ?"  fullWidth />
+        <ImageInput source="pair:depictedBy" label="Ta photo ! (C'est important pour la convivialité de la plateforme)" accept="image/*">
           <ImageField source="src" />
         </ImageInput>
-        <MarkdownInput source="cdlt:intentions" fullWidth />
+        <SectorsInput source="pair:hasSector" label="Il/elle s'intéresses à des secteurs d'activité en particulier ?" />
+        <TopicsInput source="pair:hasTopic" label="C'est quoi ses centres d'intérêts ?" />
+        <SkillsInput source="pair:offers" label="C\'est quoi ses compétences actuelles ?" />
+        <SkillsInput source="pair:aims" label="Quelles sont les compétences qu'il/elle recherche ?" fullWidth />
+        <FinalitiesInput source="pair:hasFinality" label="Quelles sont les finalités qu'il/elle poursuit ?"  />
         <TextInput source="pair:phone" fullWidth helperText="Non visible sur la plateforme" />
         <TypeInput source="pair:hasType" filter={{ a: 'pair:PersonType' }} />
         <StatusInput source="pair:hasStatus" filter={{ a: 'pair:AgentStatus' }} />
@@ -55,18 +60,14 @@ export const PersonEdit = (props) => (
         */}
         <ReminderBeforeRecording />
       </FormTab>
-      <FormTab label="Relations">
-        <OrganizationsInput source="pair:affiliatedBy" />
+      <FormTab label="En lien avec lui/elle">
+        <OrganizationsInput source="pair:affiliatedBy" label="Il/elle est membre d'une (ou plusieurs) organisations ?" />
         <OrganizationsInput source="pair:inspiredBy" />
         <PlacesInput source="cdlt:proposes" />
         <ActivitiesInput source="cdlt:mentorOn" />
         <ActivitiesInput source="cdlt:organizes" />
-        <SectorsInput source="pair:hasSector" />
-        <TopicsInput source="pair:hasTopic" />
+
         <PathsInput source="cdlt:supports" />
-        <SkillsInput source="pair:offers" />
-        <SkillsInput source="pair:aims" fullWidth />
-        <FinalitiesInput source="pair:hasFinality" />
       </FormTab>
     </TabbedForm>
   </Edit>
