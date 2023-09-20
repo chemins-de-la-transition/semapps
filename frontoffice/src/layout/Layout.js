d92ef7b838b8e57988deb0a660224fded69b6f2c
Tonfa <abdulrahman.otman@gmail.com>
2023-09-20 19:20:40 +0200

    fix / in library page

--- frontoffice/src/layout/Layout.js
+++ frontoffice/src/layout/Layout.js
@@ -13,7 +13,7 @@ const Layout = ({ logout, theme, children, title }) => {
     { link: '/Path', name: translate('app.nav.path') },
     { link: '/Place', name: translate('app.nav.place') },
     { link: '/Event', name: translate('app.nav.event') },
-    { link: '/Page/https%3A%2F%2Fdata.pathways.gen-europe.org%2Fpages%2Fecommunity-library/show', name: translate('app.nav.library') },
+    { link: '/Page/https//data.pathways.gen-europe.org/pages/ecommunity-library/show', name: translate('app.nav.library') },
     //{ link: '/Course', name: translate('app.nav.course') },
   ];
   // const xs = useMediaQuery(theme.breakpoints.down('xs'));
